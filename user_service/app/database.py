beae27e164cd053dc49151d2431ce125a8e9f91c
suubha7 <suubha090@gmail.com>
2026-06-24 22:23:21 +0530

    Dockerize users service with PostgreSQL

--- user_service/app/database.py
+++ user_service/app/database.py
@@ -5,10 +5,10 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./users.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 SessionUser = sessionmaker(autocommit= False, autoflush= False, bind= engine)
 
